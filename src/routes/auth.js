9400db70c770ecf3510fab8c4218318879baeb23
PrasadK05 <kardeprasad1996@gmail.com>
2023-03-24 05:28:19 +0530

    added server code

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -8,7 +8,8 @@ const refreshToken_secret = process.env.REFRESHTOKEN_KEY;
 let cookieOption = {
   httpOnly: false,
   expires: new Date(Date.now() + 30000000),
-  secure:true
+  secure:true,
+  sameSite:"none"
 };
 
 const app = express.Router();
