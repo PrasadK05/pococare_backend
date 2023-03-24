2b70f3e6cf97104ddb33dea08f51b62b2075219e
PrasadK05 <kardeprasad1996@gmail.com>
2023-03-24 05:34:59 +0530

    added server code

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -6,7 +6,7 @@ const token_secret = process.env.TOKEN_KEY;
 const refreshToken_secret = process.env.REFRESHTOKEN_KEY;
 
 let cookieOption = {
-  httpOnly: false,
+  httpOnly: true,
   expires: new Date(Date.now() + 30000000),
   secure:true,
   sameSite:"none"
